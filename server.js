75de0c6cb509151f88b2c8ddda4eadb0584eaeb9
LuisBrasil <lsoares@byupathway.edu>
2025-11-10 22:45:40 -0700

    update

--- server.js
+++ server.js
@@ -25,6 +25,27 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  * Routes
