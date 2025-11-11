7c1cfd5fcdd00b2b86843906444d530ac381448c
LuisBrasil <lsoares@byupathway.edu>
2025-11-10 22:19:39 -0700

    error handling

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const pool = require("./database/")
-const utilities = require("./utilities/index")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require('./utilities/index')
+const session = require("express-session")
+const pool = require('./database/')
 
 
 /* ***********************
@@ -25,13 +26,57 @@ app.set("layout", "./layouts/layout") // not at views root
 
 
 /* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+/* ***********************
  * Routes
  *************************/
-app.use(require("./routes/static"))
+app.use(static)
 // Index Route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 
 /* ***********************
