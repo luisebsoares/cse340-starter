3146a63bc78281757dad1e26a337ee47c424a919
LuisBrasil <lsoares@byupathway.edu>
2025-11-10 21:11:08 -0700

    fixed errors

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const pool = require("./database/")
-const utilities = require("./utilities/")
+const utilities = require("./utilities/index")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 /* ***********************
