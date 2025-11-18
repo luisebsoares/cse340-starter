6836dcde0d4549482f4734ba9b66d37563927e8e
LuisBrasil <lsoares@byupathway.edu>
2025-11-17 20:19:14 -0700

    register view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,9 +3,11 @@ const router = new express.Router()
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
 
-
+/* deliver login view */
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+/* deliver registration view */
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 
 module.exports = router
