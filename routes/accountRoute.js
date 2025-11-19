39faa6d75da71ad12a74aa3a2c6796d11a5e9214
LuisBrasil <lsoares@byupathway.edu>
2025-11-18 18:14:16 -0700

    process registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -9,5 +9,9 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 /* deliver registration view */
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+/* process registration */
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
+
+
 
 module.exports = router
